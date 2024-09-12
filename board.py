cb06c213ec1e9f68a20ef50ba6f052c46868698a
dzsailors <143645948+dzsailors@users.noreply.github.com>
2024-09-12 18:10:32 -0500

    added code for attack() and defeat() along with comments for each line

--- board.py
+++ board.py
@@ -1,3 +1,6 @@
+from player import Player
+from game import Game
+
 class Board:
     # Ships: space that contains ship: denoted as "S" (or s1 or s2 for players one or two.. depends)
     # Empty: space w no ship: denoted _
@@ -7,7 +10,8 @@ class Board:
         # grid
         self.grid([" _" for i in range(10)])
         # hit count
-        # self.hit_count = 0
+        self.hit_count = 0
+        self.game = Game()
 
     # string representation of grid
     def __str__(self):
@@ -28,14 +32,18 @@ class Board:
 
     # attack method, record attack whether hit or miss. takes in x,y position on board
     def attack(self, x, y):
-        # if position is an "S", then mark as hit "x"
-        # you get the gist..
-        pass
+        position = self.grid[y][x] # checks what's currently at the position
+        if position == "S": # if there's a ship at the position 
+            self.grid[y][x] = " X" # changes the position from 'S' to 'X' indicating a hit
+            self.hit_count += 1 # adds one to the hit count to use later 
+            return True # communicates back that there was a hit
+        elif position == " _": # if there isn't a ship in this position
+            self.grid[y][x] = " O" # changes the position from 'S' to 'X' indicating a miss
+            return False # communicates back that there wasn't a hit
+        else: # returns false if both cases above don't work
+            return False
     
     # check if player has lost
     def defeat(self):
-        # can use the hit_count
-        pass
-
-
-
+        fleet_hit_counts = {1: 1, 2: 3, 3: 6, 4: 10, 5: 15} # this assigns the fleet sizes (1 - 5) with the number of ship hit points they'll have
+        return self.hit_count == fleet_hit_counts.get(Game.fleet_type, 0) # checks if the hit_count matches the required number of hits to defeat the fleet, depending on the value from Game.fleet_type
