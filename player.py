619fccf06532a2726fa2be9397395c43d3ccf6be
Jawad Ahsan <143557011+JawadAhsan02@users.noreply.github.com>
2024-09-29 16:09:34 -0500

    Update player.py

--- player.py
+++ player.py
@@ -20,15 +20,17 @@ class Player:
     def __init__(self, player_name, fleet):
         self.player_name = player_name
         self.board = Board()
-        self.fleet = fleet # list of fleet of ships. depends on what is chosen for the game by users. see Game.py
+        self.fleet = fleet  # list of fleet of ships. depends on what is chosen for the game by users. see Game.py
         self.opponent = None
-    
+        self.hits = 0  # Added to track player hits
+        self.misses = 0  # Added to track player misses
+
     # Input: Player object
     # Output: None
-    # Description: Sets the oppenent
+    # Description: Sets the opponent
     def set_opponent(self, opponent):
         self.opponent = opponent
-    
+
     # Inputs: None; Player is provided ships on creation
     # Outputs: None
     # Description: position ships with for loop of place_ships + board confirmation to finalize board
@@ -40,41 +42,27 @@ class Player:
         accepted = False
         while not accepted:
             for ship in self.fleet:
-                # keep prompting to place a ship until a valid position is given
                 while self.place_ship(ship) == False:
                     continue
-            # once fleet is placed, print board to for user to confirm everything is as desired
             print(self.board.player_view())
             prompt = input("Is this board okay?:\n 1. Yes\n 2. No\nChoice: ").strip()
             if prompt == '1':
                 accepted = True
-            # anything besides 'yes' is considered as 'no'
             else:
                 print("Resetting board")
-                # clear board
                 self.board.grid = [[" _" for i in range(10)] for i in range(10)]
                 print("Board reset")
-        # clear all previous text
         print("\033[H\033[J", end="")
-        
-    
-    # Input: a single ship
-    # Output: Boolean indicating whether placing the ship was successful
-    # Description: Prompt players for orientation and top left position of ship, then attempt to place ship based on those requests
+
     def place_ship(self, ship):
-        # constants
         HORIZONTAL_SELECTED = '1'
         VERTICAL_SELECTED = '2'
 
-        # show board as is 
         print("Current board: ")
         print(self.board.player_view())
-
         print("Placing ship with length ", ship.size)
-        # ask orientation horizontal or vertical
+
         valid_orientation_selected = False
-        # keep prompting for orientation until '1' or '2' is entered
-        # then set orientation
         while not valid_orientation_selected:
             orientation = input("Choose Orientation:\n 1. Horizontal\n 2. Vertical \nPlayer choice: ").strip()
             if orientation == HORIZONTAL_SELECTED:
@@ -86,23 +74,16 @@ class Player:
             else:
                 print("Invalid orientation")
 
-
-        # prompt for and set top left position of boat
         col_valid = False
         col = ''
-        # keep prompting until valid column given
         while not col_valid:
             col = input("Enter column from A-J: ").strip()
-            # Python supports comparisons of strings based on their ASCII number, so this is okay despite how strange it looks
-            # only accept 1 letter answers to avoid any confusion
             if (col >= 'A' and col <= 'J') or (col >= 'a' and col <= 'j') and (len(col) < 2):
-                # gives a number 0-10 ('a' ascii value is 97, successive letters go up by 1 in alphabetical order)
                 ship.x = ord(col.lower()) - 97
                 col_valid = True
             else:
                 print("Invalid column")
         
-        # keep prompting until row column given
         row_valid = False
         row = 0
         while not row_valid:
@@ -111,42 +92,28 @@ class Player:
                 if row < 1 or row > 10:
                     print("Invalid row number")
                 else:
-                    # -1 to make it easier to work with 0-indexed lists
                     ship.y = row-1
                     row_valid = True
-            # non-integer provided
             except ValueError:
                 print("Invalid row number")
 
-
-        # use place_ship from board.py
         return self.board.place_ship(ship)
 
-    # Input: The type of fleet, which indicates size and number of ships
-    # Output: boolean indicating whether the current player has won
-    # Description: prompt for position on board and attack
-    # Does not check if position has already been attacked
     def take_turn(self, fleet_type):
-        # check if p1/p2 ready
         print(f"{self.player_name}'s turn to attack")
         print(f"No cheating, {self.opponent.player_name}! Look away!")
         input("Press enter to begin turn")
 
-        # display boards - player board and opponent board with hits/misses shown and ships hidden
         print("Your board")
         print(self.board.player_view())
         print("Opponent's board")
         print(self.opponent.board.opponent_view())
 
-        # prompt for place to attack
         col_valid = False
         col = ''
-        # keep prompting until valid column given
         while not col_valid:
             col = input("Enter column from A-J: ").strip()
-            # Python supports comparisons of strings based on their ASCII number, so this is okay despite how strange it looks
             if (col >= 'A' and col <= 'J') or (col >= 'a' and col <= 'j') and (len(col) < 2):
-                # gives a number 0-10 ('a' ascii value is 97, successive letters go up by 1 in alphabetical order)
                 col = ord(col.lower()) - 97
                 col_valid = True
             else:
@@ -154,31 +121,27 @@ class Player:
         
         row_valid = False
         row = 0
-        # keep prompting until valid row given
         while not row_valid:
             try:
                 row = int(input("Enter row from 1-10: "))
                 if row < 1 or row > 10:
                     print("Invalid row number")
                 else:
-                    # -1 to make it easier to work with 0-indexed lists
                     row = row-1
                     row_valid = True
             except ValueError:
                 print("Invalid row number")
 
-        # attack and indiciate if hit or miss
-        if (self.opponent.board.attack(col, row)):
+        if self.opponent.board.attack(col, row):
             print("Hit!")
+            self.hits += 1  # Track hit
         else:
             print("Miss!")
+            self.misses += 1  # Track miss
 
-        # check for opponent defeat
         if self.opponent.board.defeat(fleet_type):
             return True
         else:
-            # game continues
-            # clear all previous text
             input("Press enter to end turn")
             print("\033[H\033[J", end="")
             return False
