8ae1f41c6ba77339a3996a92be91da75ff2dc05d
Jawad Ahsan <143557011+JawadAhsan02@users.noreply.github.com>
2024-09-29 16:08:56 -0500

    Update main.py
    updated to add the score card

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ Author: Anakha Krishna
 Creation Date: 9/10/24
 Last Modified: 9/15/24
 Commenting: All comments written on 9/15/24 by Anakha Krishna and Isabel Loney
-Program Name: Battleship - game.py
+Program Name: Battleship - main.py
 Purpose: Entry point for running the battleship game. Creates the game and begins it
 Source(s): https://ils.unc.edu/courses/2017_spring/inls560_001/a/battleship.py for overall program structure 
 Other collaborators: Code reviewed and tested by Jackson Wunderlich, modified by Isabel Loney
 '''
+
 from game import Game
 
 # Input: None
@@ -15,10 +16,13 @@ from game import Game
 # Description: create a Game object and start playing
 # Written by Anakha Krishna and Isabel Loney
 def main():
-  game = Game()
-  game.play()
+    game = Game()
+    game.play()
+    
+    # Call the display_scorecard after each round
+    game.display_scorecard()  # Show the final scorecard at the end of the game
 
 # Entry Point
 # Written by Isabel Loney
 if __name__ == "__main__":
-  main()
+    main()
